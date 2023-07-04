7a27f8a1d14794867ad335ba235fa53ed97e4e31
kvartoom <kvartoom@gmail.com>
2023-07-04 18:53:56 +0300

    4.3.5. Множественное логическое выражение И [#9461]

--- src/test/java/ru/job4j/condition/TriangleTest.java
+++ src/test/java/ru/job4j/condition/TriangleTest.java
@@ -1,9 +1,7 @@
 package ru.job4j.condition;
 
 import org.junit.jupiter.api.Test;
-
 import static org.assertj.core.api.Assertions.assertThat;
-
 import static org.junit.Assert.*;
 
 public class TriangleTest {
