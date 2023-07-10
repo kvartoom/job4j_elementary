b764c4d580b9b18ba037ee5c55b970ea805a2927
kvartoom <kvartoom@gmail.com>
2023-07-10 20:33:36 +0300

    6.0.3. Заполнение массива. [#226300]

--- src/main/java/ru/job4j/array/ArrayDefinition.java
+++ src/main/java/ru/job4j/array/ArrayDefinition.java
@@ -9,6 +9,15 @@ public class ArrayDefinition {
         System.out.println(surnames.length);
         float[] prices = new float[40];
         System.out.println(prices.length);
+        String[] names = new String[4];
+        names[0] = "Dmitrii Meshcheriakov";
+        names[1] = "Mike Tyson";
+        names[2] = "Muhammad Ali";
+        names[3] = "Arnold Schwarzenegger";
+        System.out.println(names[0]);
+        System.out.println(names[1]);
+        System.out.println(names[2]);
+        System.out.println(names[3]);
     }
 
 }
