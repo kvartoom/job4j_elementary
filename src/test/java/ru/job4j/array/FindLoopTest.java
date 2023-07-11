8e675bb7584ad4572333030593741a2738524691
kvartoom <kvartoom@gmail.com>
2023-07-11 21:07:28 +0300

    6.5. Поиск индекса в диапазоне.  [#156318]

--- src/test/java/ru/job4j/array/FindLoopTest.java
+++ src/test/java/ru/job4j/array/FindLoopTest.java
@@ -1,12 +1,13 @@
 package ru.job4j.array;
 
 import org.junit.jupiter.api.Test;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class FindLoopTest {
     @Test
     public void whenArrayHas5Then0() {
-        int[] data = new int[] {5, 10, 3};
+        int[] data = new int[]{5, 10, 3};
         int el = 5;
         int result = FindLoop.indexOf(data, el);
         int expected = 0;
@@ -15,7 +16,7 @@ public class FindLoopTest {
 
     @Test
     public void whenDiapasonHas8ThenResultEqualFinish() {
-        int[] data = new int[] {5, 2, 10, 2, 4, 8, 14, 3, 21, 16};
+        int[] data = new int[]{5, 2, 10, 2, 4, 8, 14, 3, 21, 16};
         int el = 8;
         int start = 2;
         int finish = 5;
@@ -26,7 +27,7 @@ public class FindLoopTest {
 
     @Test
     public void whenDiapasonHasSomeEqualElementThen3() {
-        int[] data = new int[] {5, 10, 2, 4, 8, 4, 14, 4, 3, 21, 16};
+        int[] data = new int[]{5, 10, 2, 4, 8, 4, 14, 4, 3, 21, 16};
         int el = 4;
         int start = 1;
         int finish = 8;
@@ -37,7 +38,7 @@ public class FindLoopTest {
 
     @Test
     public void whenDiapasonHas2Then3() {
-        int[] data = new int[] {5, 2, 10, 2, 4};
+        int[] data = new int[]{5, 2, 10, 2, 4};
         int el = 2;
         int start = 2;
         int finish = 4;
@@ -48,7 +49,7 @@ public class FindLoopTest {
 
     @Test
     public void whenDiapasonHasNot8ThenMinus1() {
-        int[] data = new int[] {6, 4, 3, 7, 9, 5, 8};
+        int[] data = new int[]{6, 4, 3, 7, 9, 5, 8};
         int el = 8;
         int start = 2;
         int finish = 5;
