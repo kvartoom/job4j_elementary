397f210478d277e7886b1e669309c238a97ce713
kvartoom <kvartoom@gmail.com>
2023-08-03 20:49:17 +0300

    SimpleStringEncoder1

--- src/main/java/ru/job4j/array/StringUsage.java
+++ src/main/java/ru/job4j/array/StringUsage.java
@@ -9,12 +9,12 @@ public class StringUsage {
             if (input.charAt(i) == symbol) {
                 counter++;
             } else {
-                result =  result + symbol + (counter == 1 ? "" : counter);
+                result = result + symbol + (counter == 1 ? "" : counter);
                 symbol = input.charAt(i);
                 counter = 1;
             }
         }
-        result =  result + symbol + (counter == 1 ? "" : counter);
+        result = result + symbol + (counter == 1 ? "" : counter);
         return result;
     }
 }
